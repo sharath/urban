1e8d88e7c18023d61c15d59f57efc77ee5b243b2
NeoMax97 <sid10rox4ever@gmail.com>
2018-10-13 02:23:09 -0400

    Setup of tutorial files

--- app/src/main/java/com/thisdomainhasnotbeentakenyet/urban/MainActivity.java
+++ app/src/main/java/com/thisdomainhasnotbeentakenyet/urban/MainActivity.java
@@ -2,12 +2,35 @@ package com.thisdomainhasnotbeentakenyet.urban;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.tv_hw);
+
+        Button button = findViewById(R.id.b_ct);
+        // button.setOnClickListener(this);     Goes to onClick method
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.isPressed()) {
+                    textView.setText(R.string.gw);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.b_ct){
+            // TextView has to be global to interact here.
+        }
     }
 }
